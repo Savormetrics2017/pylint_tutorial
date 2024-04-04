89d6fc97f00bc2367440f38a57f6ce76839fbfec
kanav-mirakalous <kanav@mirakalous.com>
2024-04-04 12:29:01 -0400

    Initial commit

---
+++ good_pylint.py
@@ -0,0 +1,14 @@
+"""
+This is a module docstring. It explains what this module is about.
+"""
+def good_name():
+    """
+    This is a function docstring. It explains what this function does.
+    """
+    meaningful_variable = 1
+    if meaningful_variable == 1:
+        print('good indentation')
+    return meaningful_variable
+
+if __name__ == "__main__":
+    good_name()
